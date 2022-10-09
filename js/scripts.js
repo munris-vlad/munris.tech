befd4457138418bb5cb89f4750b206b008986434
Vladylsav Palchykov <vladyslav.palchykov@onix-systems.com>
2022-10-09 20:46:04 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -711,25 +711,6 @@ $(document).ready(function () {
 
 /* ===============================  Mouse effect  =============================== */
 
-function mousecursor() {
-    if ($("body")) {
-        const e = document.querySelector(".cursor-inner"),
-            t = document.querySelector(".cursor-outer");
-        let n, i = 0,
-            o = !1;
-        window.onmousemove = function (s) {
-            o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
-        }, $("body").on("mouseenter", "a, .cursor-pointer", function () {
-            e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
-        }), $("body").on("mouseleave", "a, .cursor-pointer", function () {
-            $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
-        }), e.style.visibility = "visible", t.style.visibility = "visible"
-    }
-};
-
-$(function () {
-    mousecursor();
-});
 
 /* ===============================  fixed-slider  =============================== */
 
